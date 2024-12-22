48327ed7c5e240ab91933e47093dac1e1aae75dd
Ahmed <AhmedMuhammed0031@gmail.com>
2024-12-22 13:39:07 +0300

    Day 16

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,37 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $job = Job::with('employer')->simplePaginate(2);
+    $job = Job::with('employer')->latest()->simplePaginate(2);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $job,
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
+
+Route::post('/jobs', function () {
+    request()->validate([
+        'title'  => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+    Job::create([
+        'title'       => request('title'),
+        'salary'      => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
+});
+
 
 Route::get('/contact', function () {
     return view('contact');
